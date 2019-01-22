6624e7ffeb4d2e0a22f092e0307dc21d0ad6e620
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-01-22 16:57:34 +0300

    [finishes#163342287] configuring database

---
+++ app/db_conn.py
@@ -0,0 +1,87 @@
+import psycopg2
+import os  
+
+url="dbname='questioner_api' host='localhost'  port='5432' user='postgres' password='andela'"
+
+db_url=os.getenv('DATABASE_URL')
+
+print(db_url)
+
+conn =psycopg2.connect(url)
+
+cur = conn.cursor()
+
+
+    
+cur.execute(
+      '''CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY NOT NULL,
+        firstname VARCHAR(250) NOT NULL,
+        lastname VARCHAR(250) NOT NULL,
+        othername VARCHAR(250) NULL,
+        username VARCHAR(250) NOT NULL,
+        phonenumber VARCHAR(250) NULL,
+        email VARCHAR(250) NOT NULL,
+        password VARCHAR(250) NOT NULL,
+        registered TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        admin BOOLEAN NOT NULL DEFAULT FALSE
+    );
+    CREATE TABLE IF NOT EXISTS meetups (
+        id SERIAL PRIMARY KEY NOT NULL,
+        topic VARCHAR(250) NOT NULL,
+        description VARCHAR(250) NOT NULL,
+        location VARCHAR(250) NOT NULL,
+        happening_on VARCHAR(250) NOT NULL,
+        tags VARCHAR [],
+        created_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        modified_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc')
+    );
+     CREATE TABLE IF NOT EXISTS questions (
+        id SERIAL PRIMARY KEY NOT NULL,
+        title VARCHAR(250) NULL,
+        body VARCHAR(250) NOT NULL,
+        votes INTEGER NOT NULL DEFAULT 0,
+        meetup_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        created_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        modified_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        FOREIGN KEY (meetup_id) REFERENCES meetups(id),
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    );
+    CREATE TABLE IF NOT EXISTS comments (
+        id SERIAL PRIMARY KEY NOT NULL,
+        body VARCHAR(250) NULL,
+        question_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        created_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        modified_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        FOREIGN KEY (question_id) REFERENCES questions(id),
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    );
+     CREATE TABLE IF NOT EXISTS rsvps (
+        meetup_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        response VARCHAR(10),
+        created_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        modified_at TIMESTAMP WITHOUT TIME ZONE \
+        DEFAULT (NOW() AT TIME ZONE 'utc'),
+        PRIMARY KEY (meetup_id, user_id)
+    );
+    CREATE TABLE IF NOT EXISTS votes (
+        question_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        vote VARCHAR(10),
+        PRIMARY KEY (question_id, user_id)
+    );
+    ''')
+
+conn.commit()
+conn.close()
---
+++ instance/config.py
@@ -0,0 +1,50 @@
+import os
+
+class Config(object):
+    """ Base Config class """
+
+    DEBUG = False
+    TESTING = False
+    DATABASE_HOST = os.getenv('DATABASE_HOST')
+    DATABASE_PORT = os.getenv('DATABASE_PORT')
+    DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+   
+
+class DevelopmentConfig(Config):
+    """ Config class for Development environment """
+
+    DEBUG = True
+    DATABASE_NAME = os.getenv('DATABASE_NAME')
+
+
+class TestingConfig(Config):
+    """ Config class for Testing environment """
+
+    TESTING = True
+    DATABASE_NAME = os.getenv('TEST_DATABASE_NAME')
+
+
+class StagingConfig(Config):
+    """ Config class for Staging environment """
+
+    DEBUG = True
+    DATABASE_NAME = os.getenv('TEST_DATABASE_NAME')
+
+
+class ProductionConfig(Config):
+    """ Config class for Production environment """
+
+    DEBUG = False
+    TESTING = False
+    DATABASE_NAME = os.getenv('DATABASE_NAME')
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig
+}
+
