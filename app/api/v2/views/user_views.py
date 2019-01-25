a34bf1c199cae3c751444044ad13696a81fdf977
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-01-25 04:03:27 +0300

    [finishes#163342505] user login

--- app/api/v2/views/user_views.py
+++ app/api/v2/views/user_views.py
@@ -23,6 +23,15 @@ def register():
     data, errors = UserSchema().load(json_data)
     if errors:
         return jsonify({'status': 400, 'message' : 'Invalid data. Please fill all the required fields', 'errors': errors}), 400
+    
+
+    # Checking  if  the username exists
+    if db.exists('username', data['username']):
+        return jsonify({'status': 409, 'message' : 'Username already exists'}), 409
+
+    # Checking  if the  email exists
+    if db.exists('email', data['email']):
+        return jsonify({'status': 409, 'message' : 'Email already exists'}), 409
 
 
     # Save new user and get result
@@ -36,3 +45,40 @@ def register():
         'data': result, 
        
         }), 201
+
+
+@v2.route('/login', methods=['POST'])
+def login():
+    json_data = request.get_json()
+
+    # Check if the request contains any data
+    if not json_data:
+        return jsonify({'status': 400, 'message': 'No data has provided! Please put your login credentials'}), 400
+
+    # Check if credentials have been passed
+    data, errors = UserSchema().load(json_data, partial=True)
+    if errors:
+        return jsonify({'status': 400, 'message': 'Invalid data. Please fill all required fields', 'errors': errors}), 400
+
+    try:
+        username = data['username']
+        password = data['password']
+    except:
+        return jsonify({'status': 400, 'message': 'Invalid credentials.Confirm!'}), 400
+
+    # Check if username exists
+    if not db.exists('username', username):
+        return jsonify({'status': 404, 'message' : 'User not found'}), 404
+
+    user = db.find('username', username)
+
+    # Checking if password match
+    db.checkpassword(user['password'], password)
+
+
+   
+    return jsonify({
+        'status': 200, 
+        'message': 'User logged in successfully',
+       
+        }), 200
