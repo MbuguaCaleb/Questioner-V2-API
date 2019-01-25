4e785aa5f28f0e49387752fb7d66ca2cdac389df
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-01-25 04:02:49 +0300

    [starts#163342505] user login

--- app/api/v2/models/user_model.py
+++ app/api/v2/models/user_model.py
@@ -1,6 +1,6 @@
 from ....db_conn import initialize_db
 from psycopg2.extras import RealDictCursor
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 con = initialize_db()
 cur = con.cursor(cursor_factory=RealDictCursor)
@@ -8,6 +8,8 @@ cur = con.cursor(cursor_factory=RealDictCursor)
 
 class User(object):
     """ Model class for the user object """
+
+    table ='users'
 
     def save(self, data=None):
 
@@ -18,4 +20,32 @@ class User(object):
         return data
 
         
-  
+     
+    def exists(self, key, value):
+        """ Function to check if user exists """
+
+        query = "SELECT * FROM {} WHERE {} = '{}'".format(
+            self.table, key, value)
+        cur.execute(query)
+
+        result = cur.fetchall()
+        return len(result) > 0
+    
+
+    def find(self, key, value):
+        
+
+        """it checks whether the user exists from the DB"""
+
+        query = "SELECT * FROM {} WHERE {} = '{}'".format(
+            self.table, key, value)
+        cur.execute(query)
+        result = cur.fetchone()
+        return result
+
+
+    @staticmethod
+    def checkpassword(hashed_password, password):
+        """ Function to check if passwords match """
+
+        return check_password_hash(hashed_password, password)
