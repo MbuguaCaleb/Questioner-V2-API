9aab8c1a05ddf8d9684a6642b5194d44fe8c6ea4
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-01-25 06:12:03 +0300

    [finishes#163342424] modifies v2 blueprint

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,5 +1,5 @@
-""" Initiates blueprints for version 1 of the API """
+""" Initiates blueprints for version 2 of the API """
 from flask import Blueprint
 
-version_2 = Blueprint('version_one', __name__, url_prefix='/api/v2')
+version_2 = Blueprint('version_2', __name__, url_prefix='/api/v2')
 
